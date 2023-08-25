7a021f279c1624afd2fe92f282103d6c35027425
Raushan Kumar <sraushan191@gmail.com>
2023-08-25 18:20:46 +0530

    Add files via upload

---
+++ 04project.js
@@ -0,0 +1,24 @@
+const randomcolor=function() {
+    const hex="123456789ABCDEF"
+    let color="#"
+    for(let i=0;i<6;i++)
+    {
+        color +=Math.floor(Math.random()*16)
+    }
+    return color;
+}
+let intervalId
+const startchangingcolor=function(){
+   intervalId= setInterval(changebgcolor,1000)
+
+    function changebgcolor()
+    {
+     document.body.style.backgroundColor=randomcolor()
+    }
+};
+const stopchangingcolor=function(){
+    clearInterval(intervalId);
+    
+};
+document.querySelector("#start").addEventListener('click',startchangingcolor);
+document.querySelector("#stop").addEventListener('click',stopchangingcolor);
